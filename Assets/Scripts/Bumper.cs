a6be45200c399762964ac6ae77cb523f5ace59c9
otrapersona <otrapersonagit@riseup.net>
2021-05-26 20:43:29 -0500

    bumper rounded to 1 digit

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -60,7 +60,7 @@ public class Bumper : MonoBehaviour
             mousePos.y = Input.mousePosition.y;
             mousePos.z = camNearClipPlane - camPosZ;
             point = cam.ScreenToWorldPoint(mousePos);
-            transform.position = new Vector3((float)System.Math.Round(point.x,3), (float)System.Math.Round(point.y,3), (float)System.Math.Round(point.z,3));
+            transform.position = new Vector3((float)System.Math.Round(point.x,1), (float)System.Math.Round(point.y,1), (float)System.Math.Round(point.z,1));
         }
     }
     private void OnMouseUp()
